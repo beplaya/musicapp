f3a19cf91fefbbfde37f6ae62cf4d24334b1e13b
beplaya <beplaya@gmail.com>
2017-07-05 18:00:54 -0400

    wip search api model

--- app/src/main/java/examples/gjames/com/musicapp/dal/apis/itunes/models/ItunesSearchResult.java
+++ app/src/main/java/examples/gjames/com/musicapp/dal/apis/itunes/models/ItunesSearchResult.java
@@ -1,4 +1,259 @@
 package examples.gjames.com.musicapp.dal.apis.itunes.models;
 
+import java.util.Date;
+
 public class ItunesSearchResult {
+
+    private String wrapperType;
+    private String kind;
+    private int artistId;
+    private int collectionId;
+    private int trackId;
+    private String artistName;
+    private String trackCensoredName;
+    private String artistViewUrl;
+    private String collectionViewUrl;
+    private String trackViewUrl;
+    private String previewUrl;
+    private String artworkUrl30;
+    private String artworkUrl60;
+    private String artworkUrl100;
+    private float collectionPrice;
+    private float trackPrice;
+    private Date releaseDate;
+    private String collectionExplicitness;
+    private String trackExplicitness;
+    private int discCount;
+    private int discNumber;
+    private int trackCount;
+    private int trackNumber;
+    private float trackTimeMillis;
+    private String country;
+    private String currency;
+    private String primaryGenreName;
+    private boolean isStreamable;
+
+    public String getWrapperType() {
+        return wrapperType;
+    }
+
+    public void setWrapperType(String wrapperType) {
+        this.wrapperType = wrapperType;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public int getArtistId() {
+        return artistId;
+    }
+
+    public void setArtistId(int artistId) {
+        this.artistId = artistId;
+    }
+
+    public int getCollectionId() {
+        return collectionId;
+    }
+
+    public void setCollectionId(int collectionId) {
+        this.collectionId = collectionId;
+    }
+
+    public int getTrackId() {
+        return trackId;
+    }
+
+    public void setTrackId(int trackId) {
+        this.trackId = trackId;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getTrackCensoredName() {
+        return trackCensoredName;
+    }
+
+    public void setTrackCensoredName(String trackCensoredName) {
+        this.trackCensoredName = trackCensoredName;
+    }
+
+    public String getArtistViewUrl() {
+        return artistViewUrl;
+    }
+
+    public void setArtistViewUrl(String artistViewUrl) {
+        this.artistViewUrl = artistViewUrl;
+    }
+
+    public String getCollectionViewUrl() {
+        return collectionViewUrl;
+    }
+
+    public void setCollectionViewUrl(String collectionViewUrl) {
+        this.collectionViewUrl = collectionViewUrl;
+    }
+
+    public String getTrackViewUrl() {
+        return trackViewUrl;
+    }
+
+    public void setTrackViewUrl(String trackViewUrl) {
+        this.trackViewUrl = trackViewUrl;
+    }
+
+    public String getPreviewUrl() {
+        return previewUrl;
+    }
+
+    public void setPreviewUrl(String previewUrl) {
+        this.previewUrl = previewUrl;
+    }
+
+    public String getArtworkUrl30() {
+        return artworkUrl30;
+    }
+
+    public void setArtworkUrl30(String artworkUrl30) {
+        this.artworkUrl30 = artworkUrl30;
+    }
+
+    public String getArtworkUrl60() {
+        return artworkUrl60;
+    }
+
+    public void setArtworkUrl60(String artworkUrl60) {
+        this.artworkUrl60 = artworkUrl60;
+    }
+
+    public String getArtworkUrl100() {
+        return artworkUrl100;
+    }
+
+    public void setArtworkUrl100(String artworkUrl100) {
+        this.artworkUrl100 = artworkUrl100;
+    }
+
+    public float getCollectionPrice() {
+        return collectionPrice;
+    }
+
+    public void setCollectionPrice(float collectionPrice) {
+        this.collectionPrice = collectionPrice;
+    }
+
+    public float getTrackPrice() {
+        return trackPrice;
+    }
+
+    public void setTrackPrice(float trackPrice) {
+        this.trackPrice = trackPrice;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getCollectionExplicitness() {
+        return collectionExplicitness;
+    }
+
+    public void setCollectionExplicitness(String collectionExplicitness) {
+        this.collectionExplicitness = collectionExplicitness;
+    }
+
+    public String getTrackExplicitness() {
+        return trackExplicitness;
+    }
+
+    public void setTrackExplicitness(String trackExplicitness) {
+        this.trackExplicitness = trackExplicitness;
+    }
+
+    public int getDiscCount() {
+        return discCount;
+    }
+
+    public void setDiscCount(int discCount) {
+        this.discCount = discCount;
+    }
+
+    public int getDiscNumber() {
+        return discNumber;
+    }
+
+    public void setDiscNumber(int discNumber) {
+        this.discNumber = discNumber;
+    }
+
+    public int getTrackCount() {
+        return trackCount;
+    }
+
+    public void setTrackCount(int trackCount) {
+        this.trackCount = trackCount;
+    }
+
+    public int getTrackNumber() {
+        return trackNumber;
+    }
+
+    public void setTrackNumber(int trackNumber) {
+        this.trackNumber = trackNumber;
+    }
+
+    public float getTrackTimeMillis() {
+        return trackTimeMillis;
+    }
+
+    public void setTrackTimeMillis(float trackTimeMillis) {
+        this.trackTimeMillis = trackTimeMillis;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getPrimaryGenreName() {
+        return primaryGenreName;
+    }
+
+    public void setPrimaryGenreName(String primaryGenreName) {
+        this.primaryGenreName = primaryGenreName;
+    }
+
+    public boolean isStreamable() {
+        return isStreamable;
+    }
+
+    public void setStreamable(boolean streamable) {
+        isStreamable = streamable;
+    }
 }
