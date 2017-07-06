c76141896975020636724de8c2ed31da7c1cae8a
beplaya <beplaya@gmail.com>
2017-07-06 15:46:01 -0400

    wip lyrics

--- app/src/main/java/examples/gjames/com/musicapp/_dal/apis/lyricswikia/models/Lyrics.java
+++ app/src/main/java/examples/gjames/com/musicapp/_dal/apis/lyricswikia/models/Lyrics.java
@@ -1,4 +1,10 @@
 package examples.gjames.com.musicapp._dal.apis.lyricswikia.models;
 
 public class Lyrics {
+    public String artist;
+    public String song;
+    public String lyrics;
+    public String url;
+
+
 }
--- app/src/main/java/examples/gjames/com/musicapp/controllers/lyrics/LyricsController.java
+++ app/src/main/java/examples/gjames/com/musicapp/controllers/lyrics/LyricsController.java
@@ -2,20 +2,30 @@ package examples.gjames.com.musicapp.controllers.lyrics;
 
 import android.util.Log;
 
+import examples.gjames.com.musicapp._dal.apis.lyricswikia.LyricsSearcher;
+import examples.gjames.com.musicapp._dal.apis.lyricswikia.models.Lyrics;
 import examples.gjames.com.musicapp.activities._MusicAppActivity;
 import examples.gjames.com.musicapp.controllers.ItunesSearchResultTransport;
 import examples.gjames.com.musicapp.controllers._MusicAppController;
 import examples.gjames.com.musicapp.views.lyrics.LyricsView;
 import james.a.grant.wagon.Wagon;
 
-public class LyricsController extends _MusicAppController<LyricsView> {
+public class LyricsController extends _MusicAppController<LyricsView> implements LyricsSearcher.LyricsSearcherListener {
     public ItunesSearchResultTransport transport = new ItunesSearchResultTransport();
+    private LyricsSearcher lyricsSearcher;
 
     @Override
     public void init(_MusicAppActivity activity) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
         super.init(activity);
         new Wagon<ItunesSearchResultTransport>(transport.getClass(), transport).unpack(activity.getIntent());
         getView().update(transport.selectedItunesSearchResult);
-        Log.i("sad","asd");
+
+        lyricsSearcher  = new LyricsSearcher(this);
+        lyricsSearcher.search(transport.selectedItunesSearchResult);
+    }
+
+    @Override
+    public void onLyricsResults(Lyrics lyrics, boolean error) {
+
     }
 }
